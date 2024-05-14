6f5112503360da94d4a0a5b87fbdbdfcb031c28e
DrunkenAlcoholic <drunkenalcoholic@gmail.com>
2024-05-15 03:09:07 +0800

    Fonts & Colours (#1)
    Modified digits and added colours for improved visibility

--- countdown/src/countdown.nim
+++ countdown/src/countdown.nim
@@ -1,92 +1,94 @@
 import std/[cmdline, os, strformat, strutils, tables, times, terminal]
 
 const digit0 = [
-  " 0 ",
-  "0 0",
-  "0 0",
-  "0 0",
-  " 0 "
+ " ██████ ",
+ "██  ████",
+ "██ ██ ██",
+ "████  ██",
+ " ██████ "
 ]
 
 const digit1 = [
-  "11 ",
-  " 1 ",
-  " 1 ",
-  " 1 ",
-  "111"
+ " ██",
+ "███",
+ " ██",
+ " ██",
+ " ██"
 ]
 
 const digit2 = [
-  "22 ",
-  "  2",
-  " 2",
-  "2  ",
-  "222"
+  "██████ ",
+  "     ██",
+  " █████ ",
+  "██     ",
+  "███████"
 ]
 
 const digit3 = [
-  "33 ",
-  "  3",
-  "33 ",
-  "  3",
-  "33 "
+  "██████ ",
+  "     ██",
+  " █████ ",
+  "     ██",
+  "██████ "
 ]
 
 const digit4 = [
-  "4 4",
-  "4 4",
-  "444",
-  "  4",
-  "  4"
+  "██   ██",
+  "██   ██",
+  "███████",
+  "     ██",
+  "     ██"
 ]
 
 const digit5 = [
-  "555",
-  "5  ",
-  "55 ",
-  "  5",
-  "55 "
+  "███████",
+  "██     ",
+  "███████",
+  "     ██",
+  "███████"
 ]
 
 const digit6 = [
-  " 66",
-  "6  ",
-  "66 ",
-  "6 6",
-  " 6 "
+  " ██████ ",
+  "██      ",
+  "███████ ",
+  "██    ██",
+  " ██████ "
 ]
 
 const digit7 = [
-  "777",
-  "  7",
-  " 7 ",
-  "7  ",
-  "7  "
+  "███████",
+  "     ██",
+  "    ██ ",
+  "   ██  ",
+  "   ██  "
 ]
 
 const digit8 = [
-  " 8 ",
-  "8 8",
-  " 8 ",
-  "8 8",
-  " 8 "
+  " █████ ",
+  "██   ██",
+  " █████ ",
+  "██   ██",
+  " █████ "
 ]
 
 const digit9 = [
-  " 9 ",
-  "9 9",
-  " 99",
-  "  9",
-  " 9 "
+  " █████ ",
+  "██   ██",
+  " ██████",
+  "     ██",
+  " █████ "
 ]
 
 const digitColon = [
   "   ",
-  " + ",
+  "██ ",
   "   ",
-  " + ",
+  "██ ",
   "   "
 ]
+
+const colors = { "blue": "\x1b[38;2;94;129;172m", "green": "\x1b[38;2;163;190;140m", "purple": "\x1b[38;2;180;142;173m", "orange": "\x1b[38;2;235;203;139m", "red": "\x1b[38;2;191;97;106m", "white": "\x1b[38;2;236;239;244m"}.toTable
 
 const digits = { '0': digit0, '1': digit1, '2': digit2, '3': digit3, '4': digit4, '5': digit5, '6': digit6, '7': digit7, '8': digit8, '9': digit9, ':': digitColon }.toTable
 
@@ -99,7 +101,7 @@ proc displayDigits(text: string, line: int): string =
 let params = commandLineParams()
 let requestedMinutes = if params.len > 0: params[0].parseInt() else: 5
 
-const message = "Ben will be back in:"
+const message = colors["blue"] & "Ben will be back in:"
 let startTime = now()
 let countdownTime = initDuration(seconds = requestedMinutes * 60)
 var elapsedTime = initDuration()
@@ -112,7 +114,7 @@ proc displayMessage(message: string, minutes: int, seconds: int) =
 
   for line in 0..4:
     stdout.setCursorPos 0, line + 2
-    stdout.styledWriteLine displayDigits(fmt"{minutes:02}:{seconds:02}", line)
+    stdout.styledWriteLine colors["purple"] & displayDigits(fmt"{minutes:02}:{seconds:02}", line)
 
 while countdownTime > elapsedTime:
   sleep(1000)
