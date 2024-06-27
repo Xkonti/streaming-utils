905b64b57cbc4bb1851c92c3cb38f9a1c7900289
Xkonti <xkonti@xkonti.tech>
2024-06-27 00:49:57 -0400

    Handle errors when starting twitch/youtube chat

--- overlay-server/src/index.ts
+++ overlay-server/src/index.ts
@@ -4,15 +4,28 @@ import {initYoutube} from "./chat/youtube";
 import { addChatEndpoints } from "./chat/endpoints";
 import { cors } from '@elysiajs/cors'
 
+// Initialize API
 const app = new Elysia()
     .use(cors())
     .get("/", () => "Oh, hi there!");
 addChatEndpoints(app);
 app.listen(3000);
 
-initTwitch();
-await initYoutube('ZGsO3BB-0rM');
+// Initialize Twitch chat
+try {
+  await initTwitch();
+} catch (e) {
+  console.log("Failed to initialize Twitch chat");
+}
+
+// Initialize YouTube chat
+try {
+  await initYoutube('zm7C-U0G62U'); 
+} catch (e) {
+  console.log("Failed to initialize YouTube chat");
+}
+
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `Overlay server is running at ${app.server?.hostname}:${app.server?.port}`
 );
