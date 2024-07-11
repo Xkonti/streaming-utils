0a0dc04762a884c82d97bd7a3da78d260475ec96
Xkonti <xkonti@xkonti.tech>
2024-07-10 23:24:28 -0400

    Finish the dasbboard

--- overlay/nuxt.config.ts
+++ overlay/nuxt.config.ts
@@ -1,23 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['@/assets/custom.scss'],
+  css: ["@/assets/custom.scss"],
   modules: ["nuxt-quasar-ui", "@nuxt/eslint"],
 
   quasar: {
     plugins: [
-      'BottomSheet',
-      'Dialog',
-      'Loading',
-      'LoadingBar',
-      'Notify',
-      'Dark',
+      "BottomSheet",
+      "Dialog",
+      "Loading",
+      "LoadingBar",
+      "Notify",
+      "Dark",
     ],
     extras: {
-      font: 'roboto-font',
+      font: "roboto-font",
     },
-    iconSet: 'material-icons',
-    
   },
-})
+});
---
+++ overlay/pages/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page padding class="q-gutter-sm">
+    <client-only>
+      <TwitchStatusCard />
+      <YoutubeStatusCard />
+    </client-only>
+  </q-page>
+</template>
+
+<script setup></script>
